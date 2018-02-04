448e70510dd899f22783f19b67f328718b8f18fc
isysoi3 <14ilya12322@gmail.com>
2018-02-04 16:20:26 +0300

    sp lab 2

--- System programming/lab_2/lab_2/lab_2.cpp
+++ System programming/lab_2/lab_2/lab_2.cpp
@@ -1,19 +1,13 @@
 // lab_2.cpp: определяет точку входа для консольного приложения.
 //
 
-#include "stdafx.h"
-
-
 #include <windows.h>
 #include <tlhelp32.h>
 #include <tchar.h>
 
-//  Forward declarations:
 BOOL GetProcessList();
 BOOL ListProcessModules(DWORD dwPID);
 BOOL ListProcessThreads(DWORD dwOwnerPID);
-void printError(TCHAR* msg);
-int ModulesCount(DWORD dwPID);
 
 int main(void)
 {
@@ -25,114 +19,80 @@ BOOL GetProcessList()
 {
 	HANDLE hProcessSnap;
 	HANDLE hProcess;
-	PROCESSENTRY32 pe32;
-	DWORD dwPriorityClass;
+	PROCESSENTRY32 pe32; //струкура которая описывет процесс
+	DWORD dwPriorityClass; 
 
-	// Снимок всех процессов в системе.
-	//TH32CS_SNAPPROCESS - Включает все процессы в системе в снимок.
-	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //снимок всех запущенных процессов
 	if (hProcessSnap == INVALID_HANDLE_VALUE)
-	{
-		printError(TEXT("CreateToolhelp32Snapshot (of processes)"));
 		return 0;
-	}
-
-	// Размер структуры , нужно для Process32First
+	
 	pe32.dwSize = sizeof(PROCESSENTRY32);
-
-	// Получение информации о первом процессе
-	// и завершении , если не удалось
 	if (!Process32First(hProcessSnap, &pe32))
 	{
-		printError(TEXT("Process32First")); // show cause of failure
-		CloseHandle(hProcessSnap);          // clean the snapshot object
+		CloseHandle(hProcessSnap);    
 		return 0;
 	}
+	
 
-	// Теперь проход по снимку всех процессов
-	// и показ инф о каждом процессе
-	do
+	do 
 	{
-		_tprintf(TEXT("\n\n====================================================="));
+		dwPriorityClass = 0;
+		hProcess = OpenProcess(PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
+		if (hProcess != NULL){
+			dwPriorityClass = GetPriorityClass(hProcess);
+			CloseHandle(hProcess);
+		}
+		_tprintf(TEXT("====================================================="));
 		_tprintf(TEXT("\nPROCESS NAME:  %s"), pe32.szExeFile);
 		_tprintf(TEXT("\n-------------------------------------------------------"));
 		_tprintf(TEXT("\n  Process ID        = 0x%08X"), pe32.th32ProcessID);
 		_tprintf(TEXT("\n  Parent process ID = 0x%08X"), pe32.th32ParentProcessID);
 		_tprintf(TEXT("\n  Priority     = %d"), pe32.pcPriClassBase);
 		_tprintf(TEXT("\n  Thread count      = %d"), pe32.cntThreads);
-
-		// Инф о модулях и потоках
+	
 		ListProcessThreads(pe32.th32ProcessID);
 		ListProcessModules(pe32.th32ProcessID);
 
-	} while (Process32Next(hProcessSnap, &pe32));
+		system("pause");
+		system("cls");
+	} while(Process32Next(hProcessSnap, &pe32));
 
 	CloseHandle(hProcessSnap);
 	return 1;
-}
-
-int ModulesCount(DWORD dwPID)
-{
-	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
-	MODULEENTRY32 me32;
-
-	// Take a snapshot of all modules in the specified process.
-	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
-	if (hModuleSnap == INVALID_HANDLE_VALUE)
-	{
-		printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
-		return(FALSE);
-	}
-
-	// Set the size of the structure before using it.
-	me32.dwSize = sizeof(MODULEENTRY32);
-
-	// Retrieve information about the first module,
-	// and exit if unsuccessful
-	if (!Module32First(hModuleSnap, &me32))
-	{
-		printError(TEXT("Module32First"));  // show cause of failure
-		CloseHandle(hModuleSnap);           // clean the snapshot object
-		return(FALSE);
-	}
-	return me32.GlblcntUsage;
 }
 
 BOOL ListProcessModules(DWORD dwPID)
 {
 	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
 	MODULEENTRY32 me32;
+	int countModules = 0;
 
-	// Take a snapshot of all modules in the specified process.
-	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
-	if (hModuleSnap == INVALID_HANDLE_VALUE)
-	{
-		printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
-		return(FALSE);
+	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID); // снимок модулей процесса 
+	if (hModuleSnap == INVALID_HANDLE_VALUE) {
+		_tprintf(TEXT("\n     Total number of modules for process     = %d \n"), countModules);
+		return 0;
 	}
 
-	// Set the size of the structure before using it.
 	me32.dwSize = sizeof(MODULEENTRY32);
 
-	// Retrieve information about the first module,
-	// and exit if unsuccessful
 	if (!Module32First(hModuleSnap, &me32))
 	{
-		printError(TEXT("Module32First"));  // show cause of failure
-		CloseHandle(hModuleSnap);           // clean the snapshot object
-		return(FALSE);
+		CloseHandle(hModuleSnap);
+		return 0;
 	}
 
-	// Now walk the module list of the process,
-	// and display information about each module
 	do
 	{
-		_tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
-		_tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
-		_tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
-
+		//if (me32.th32ProcessID == dwPID)
+		//{
+			_tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+			_tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+			_tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+			countModules++;
+		//}
 	} while (Module32Next(hModuleSnap, &me32));
 
+	_tprintf(TEXT("\n     Total number of modules for process     = %d \n"), countModules);
 	CloseHandle(hModuleSnap);
 	return 1;
 }
@@ -142,60 +102,29 @@ BOOL ListProcessThreads(DWORD dwOwnerPID)
 	HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
 	THREADENTRY32 te32;
 
-	// Take a snapshot of all running threads  
-	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-	if (hThreadSnap == INVALID_HANDLE_VALUE)
-		return(FALSE);
 
-	// Fill in the size of the structure before using it. 
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0); // снимок потоков процесса 
+	if (hThreadSnap == INVALID_HANDLE_VALUE)
+		return 0;
+
 	te32.dwSize = sizeof(THREADENTRY32);
 
-	// Retrieve information about the first thread,
-	// and exit if unsuccessful
 	if (!Thread32First(hThreadSnap, &te32))
 	{
-		printError(TEXT("Thread32First")); // show cause of failure
-		CloseHandle(hThreadSnap);          // clean the snapshot object
+		CloseHandle(hThreadSnap); 
 		return 0;
 	}
 
-	// Now walk the thread list of the system,
-	// and display information about each thread
-	// associated with the specified process
 	do
 	{
 		if (te32.th32OwnerProcessID == dwOwnerPID)
 		{
 			_tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
 			_tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
-			_tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
 			_tprintf(TEXT("\n"));
 		}
 	} while (Thread32Next(hThreadSnap, &te32));
 
 	CloseHandle(hThreadSnap);
 	return 1;
-}
-
-void printError(TCHAR* msg)
-{
-	DWORD eNum;
-	TCHAR sysMsg[256];
-	TCHAR* p;
-
-	eNum = GetLastError();
-	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, eNum,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-		sysMsg, 256, NULL);
-
-	// Trim the end of the line and terminate it with a null
-	p = sysMsg;
-	while ((*p > 31) || (*p == 9))
-		++p;
-	do { *p-- = 0; } while ((p >= sysMsg) &&
-		((*p == '.') || (*p < 33)));
-
-	// Показ сообщения
-	_tprintf(TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg);
 }
