0ebd7f6e44c0c40a1ef4368338f5d65b7fc13d2e
Ilya Sysoi <14ilya12322@gmil.com>
2018-02-27 20:05:38 +0300

    some corrections

--- MathMod/lab1/lab1.py
+++ MathMod/lab1/lab1.py
@@ -1,35 +1,23 @@
 import math
 
-a0 = b0 = 16387
-y = 4
-m = 2 ** 31
-f = 5
-n = 1000
-k = 48
-b = [a0/m]
-c = [a0/m]
-a = [None] * n
-v = [None] * n
 
-
-def kolmogorov_test(seq,n):
+def kolmogorov_test(seq, n):
     k = 30
     delta = 0.24
     sup = 0
     for i in range(0, k):
         hits = 0
-        for j in range(0,n):
-            if seq[j] < i/k:
+        for j in range(0, n):
+            if seq[j] < i / k:
                 hits += 1
-        F = hits/n
-        if sup < abs(F - i/k):
-            sup = abs(F - i/k)
+        F = hits / n
+        if sup < abs(F - i / k):
+            sup = abs(F - i / k)
     print(math.sqrt(n) * sup)
     return True if math.sqrt(n) * sup < delta else False
 
 
-
-def pirson_test(seq,n):
+def pirson_test(seq, n):
     k = 13
     delta = 21.0
     hi = 0
@@ -37,16 +25,27 @@ def pirson_test(seq,n):
     for i in range(0, n):
         num = int(seq[i] * k)
         counting[num] += 1
-    for i in range(0,k):
-        tmp = n/k
-        hi += ((counting[i] - tmp) ** 2) / (tmp)
+    for i in range(0, k):
+        tmp = n / k
+        hi += ((counting[i] - tmp) ** 2) / tmp
     return True if hi < delta else False
 
+
+a0 = b0 = 16387
+y = 4
+m = 2 ** 31
+f = 5
+n = 1000
+k = 48
+b = [a0 / m]
+c = [a0 / m]
+a = [None] * n
+v = [None] * n
 
 for t in range(1, n):
     b.append(((b[t - 1] * b0 * m) % m) / m)
     buf = c[t - 1] * m
-    c.append(((buf ** 2 * y + buf * b0 + f) % m) / m)
+    c.append((((buf ** 2) * y + buf * b0 + f) % m) / m)
 
 for t in range(0, k):
     v[t] = b[t]
@@ -54,17 +53,16 @@ for t in range(0, k):
 for i in range(0, n - k):
     s = int(c[i] * k)
     a[i] = v[s]
-    v[s] = b[i+k]
+    v[s] = b[i + k]
 
-for i in range(n-k, n):
+for i in range(n - k, n):
     a[i] = b[i]
 
 print(b)
 print(c)
 
-print(pirson_test(b,n),pirson_test(a,n))
+print(pirson_test(b, n), pirson_test(a, n))
 
-print(kolmogorov_test(b,n),kolmogorov_test(a,n))
+print(kolmogorov_test(b, n), kolmogorov_test(a, n))
 
 print(a)
-
