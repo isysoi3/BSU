219c2814affccc3c4bfddfed318a2a34f0879a7a
Ilya Sysoi <14ilya12322@gmail.com>
2018-02-28 00:56:41 +0300

    refactoring

--- MathMod/lab1/lab1.py
+++ MathMod/lab1/lab1.py
@@ -1,6 +1,5 @@
 import math
 
-
 def kolmogorov_test(seq, n):
     k = 30
     delta = 0.24
@@ -13,7 +12,6 @@ def kolmogorov_test(seq, n):
         F = hits / n
         if sup < abs(F - i / k):
             sup = abs(F - i / k)
-    print(math.sqrt(n) * sup)
     return True if math.sqrt(n) * sup < delta else False
 
 
@@ -30,39 +28,45 @@ def pirson_test(seq, n):
         hi += ((counting[i] - tmp) ** 2) / tmp
     return True if hi < delta else False
 
+def maclaren_marsaglia(k, n, b, c ):
+    v = [None] * n
+    a = [None] * n
+    for t in range(0, k):
+        v[t] = b[t]
 
-a0 = b0 = 16387
-y = 4
-m = 2 ** 31
-f = 5
-n = 1000
-k = 48
-b = [a0 / m]
-c = [a0 / m]
-a = [None] * n
-v = [None] * n
+    for i in range(0, n - k):
+        s = int(c[i] * k)
+        a[i] = v[s]
+        v[s] = b[i + k]
 
-for t in range(1, n):
-    b.append(((b[t - 1] * b0 * m) % m) / m)
-    buf = c[t - 1] * m
-    c.append((((buf ** 2) * y + buf * b0 + f) % m) / m)
+    for i in range(n - k, n):
+        a[i] = b[i]
+    return a
 
-for t in range(0, k):
-    v[t] = b[t]
 
-for i in range(0, n - k):
-    s = int(c[i] * k)
-    a[i] = v[s]
-    v[s] = b[i + k]
 
-for i in range(n - k, n):
-    a[i] = b[i]
+def main():
+    a0 = b0 = 16387
+    y = 4
+    m = 2 ** 31
+    f = 5
+    n = 1000
+    k = 48
+    b = [a0 / m]
+    c = [a0 / m]
 
-print(b)
-print(c)
+    for t in range(1, n):
+        b.append(((b[t - 1] * b0 * m) % m) / m)
+        tmp = c[t - 1] * m
+        c.append((((tmp ** 2) * y + tmp * b0 + f) % m) / m)
 
-print(pirson_test(b, n), pirson_test(a, n))
+    a = maclaren_marsaglia(k, n ,b, c)
 
-print(kolmogorov_test(b, n), kolmogorov_test(a, n))
+    with open('out.txt', 'w') as f:
+        print(a,file=f)
+        print("Тест пирсона для 1 ", pirson_test(b, n), "\n","Тест пирсона для 2 ", pirson_test(a, n), file=f)
+        print("Тест колмогорова для 1 ", kolmogorov_test(b, n), "\n", "Тест колмогорова для 2 ", kolmogorov_test(a, n), file=f)
 
-print(a)
+
+if __name__ == "__main__":
+    main()
