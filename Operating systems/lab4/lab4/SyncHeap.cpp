06d7ecd355b5cc1de119618ba9272707f81b1d3b
Ilya Sysoi <14ilya12322@gmail.com>
2018-03-19 20:31:18 +0300

    add lab4

---
+++ Operating systems/lab4/lab4/SyncHeap.cpp
@@ -0,0 +1,70 @@
+#include "SyncHeap.h"
+
+
+SyncHeap::SyncHeap(int nSize)
+{
+	size = nSize;
+	heap = new int[size];
+	emptySemaphore = CreateSemaphore(NULL, size, size, NULL);
+	fullSemaphore = CreateSemaphore(NULL, 0, size, NULL);
+	for (int i = 0; i < size; i++)
+	{
+		heap[i] = 0;
+	}
+	InitializeCriticalSection(&csArray);
+	InitializeCriticalSection(&csConsole);
+}
+
+SyncHeap::~SyncHeap()
+{
+	delete[] heap;
+	DeleteCriticalSection(&csConsole);
+	DeleteCriticalSection(&csArray);
+}
+
+void SyncHeap::allocate(int nElement, int element)
+{
+	while (true) {
+		if (!ReleaseSemaphore(emptySemaphore, -nElement, NULL)) {
+			EnterCriticalSection(&csArray);
+			int k = nElement;
+			for (int i = 0; i < size && k >= 0; i++)
+			{
+				if (heap[i] == 0) {
+					heap[i] = element;
+					k--;
+				}
+			}
+			LeaveCriticalSection(&csArray);
+			!ReleaseSemaphore(fullSemaphore, nElement, NULL);
+			break;
+		}
+
+	}
+}
+
+void SyncHeap::free(int element)
+{
+	EnterCriticalSection(&csArray);
+	int k = 0;
+	for (int i = 0; i < size && k >= 0; i++)
+	{
+		if (heap[i] == element) {
+			heap[i] = 0;
+			k++;
+		}
+	}
+	LeaveCriticalSection(&csArray);
+	ReleaseSemaphore(emptySemaphore, k, NULL);
+	ReleaseSemaphore(fullSemaphore, -k, NULL);
+}
+
+void SyncHeap::print()
+{
+	EnterCriticalSection(&csConsole);
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << heap[i] << " ";
+	}
+	LeaveCriticalSection(&csConsole);
+}
---
+++ Operating systems/lab4/lab4/SyncHeap.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include <windows.h>
+
+class SyncHeap
+{
+private:
+	int* heap;
+	int size;
+	CRITICAL_SECTION csConsole, csArray;
+	HANDLE mutex, emptySemaphore, fullSemaphore;
+public:
+	SyncHeap(int nSize);            // nSize - ðàçìåð êó÷è
+	~SyncHeap();
+
+	void allocate(int nElement, int element);    // ïîëó÷èòü ýëåìåíòû èç êó÷è
+	void free(int element);           // âåðíóòü ýëåìåíòû â êó÷ó
+	void print();                   // ïå÷àòàåò ìàññèâ, äëÿ îòëàäêè
+};
+
