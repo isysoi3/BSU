b07efa02a832b9f7ae8ab6642d68c9cdf99eee6f
Ilya Sysoi <14ilya12322@gmail.com>
2018-04-26 22:19:30 +0300

    <<matmod-5>>: add new methods

--- MathMod/lab5/lab5.py
+++ MathMod/lab5/lab5.py
@@ -1,63 +1,104 @@
 import numpy as np
-import sympy as sp
 import random
 
+"""
+import matplotlib
+import matplotlib.pyplot as plt
+"""
 
-sp.init_printing()
-A = [[0.8, 0.2, 0.3], [-0.2, 0.5, -0.3], [-0.4, -0.2, 1.3]]
-F = [-1, 4, 5]
-B = np.eye(3)-A
 
-# функция возвращает следующий этап
-def move(currentI, P):
-    temp = random.random()
+def get_next_markov(currentI, P):
+    rand = random.random()
     i = 0
     k = 0
-    while(i < temp):
-        i += P[currentI, k]
+    while i < rand:
+        i += P[currentI][k]
         k += 1
-    return k-1
+    return k - 1
 
-#Возвращает кси
-def ksi(l, A, F, H, Pi, P):
-    temp = random.random()
-    i=0
-    k=0
-    while(i < temp):
-        i += Pi[k]
+
+def count_hi(l, A, f, h_vector, pi, p):
+    rand = random.random()
+    i = 0
+    k = 0
+    while i < rand:
+        i += pi[k]
         k += 1
-    current = k-1
-    if( H[current]==0 ):
+    current = k - 1
+    if h_vector[current] == 0:
         return 0
-    Q = H[current]/Pi[current]
-    ksi = Q*F[current]
-    for i in range(l):  
-        temp = move(current,P)
-        Q *= A[current][temp]/P[current][temp]
-        ksi += Q*F[temp]
-        current = temp
-    return ksi
+    Q = h_vector[current] / pi[current]
+    hi = Q * f[current]
+    for i in range(l):
+        next_markov = get_next_markov(current, p)
+        Q *= A[current][next_markov] / p[current][next_markov]
+        hi += Q * f[next_markov]
+        current = next_markov
+    return hi
 
-def solve(n, m, A, F):
-    dim = len(A)
-    P=np.array([[1/dim for i in range(dim)]for j in range(dim)])#матрица вероятностей прехода
-    Pi=np.array([1/dim for i in range(dim)])#вероятности начальных состояний 
-    X=[0]*dim
-    H=[0]*dim
-    for j in range(dim):        
-        H[j]=1
-        H[j-1]=0
-        for i in range(n):
-            X[j]+=ksi(m,A,F,H,Pi,P)  
-        X[j]/=n    
-    return X
+
+def monte_carlo_solving(markov_len, markov_number, A, f):
+    size = len(A)
+    p = [[1 / size] * size for _ in range(size)]
+    pi = [1 / size] * size
+    x = np.zeros(size)
+    h = np.identity(size)
+
+    for j in range(size):
+        x[j] = sum(count_hi(markov_len, A, f, h[:, j], pi, p) for _ in range(markov_number)) / markov_number
+
+    return x
+
+
+def norm(a, b):
+    return np.linalg.norm(a - b)
+
+
+def get_numbers():
+    return (2 ** x for x in range(16))
+
+
+def calculate_markov_len(func, rez, A, f):
+    return [norm(func(1000, i, A, f), rez) for i in get_numbers()]
+
+
+def calculate_iteration_number(func, rez, A, f):
+    return [norm(func(i, 1000, A, f), rez) for i in get_numbers()]
 
 
 def main():
-    print(np.linalg.solve(A, F))
-    print(solve(1000, 1000, B, F))
+    A = [[1.2, -0.4, 0.3], [0.1, 0.7, -0.2], [-0.4, 0.0, 1.4]]
+    f = [1, 2, -2]
+    B = np.eye(3) - A
+    rez = np.linalg.solve(A, f)
+    print("Точное решение:", rez)
+    print("Решение методом Монте-Карло:", monte_carlo_solving(1000, 1000, B.copy(), f))
+
+    offset_markov_len = calculate_markov_len(monte_carlo_solving, rez, B.copy(), f)
+    offset_number_iteration = calculate_iteration_number(monte_carlo_solving, rez, B.copy(), f)
+    print(offset_markov_len)
+    print(offset_number_iteration)
+    """
+    draw(change_markov_len, 0, "markov length")
+    draw(change_number_iteration , 0, "number iterations")
+    """
 
 
+"""
+def draw(real, theory, str):
+    matplotlib.rc('ytick', labelsize=15)
+    matplotlib.rc('xtick', labelsize=15)
+    plt.figure(figsize=(20, 8))
+    x = list(get_numbers())
+    plt.plot(x, [theory]*len(x), label='theory')
+    plt.plot(x, real, label='real')
+    plt.xscale('log')
+    #plt.xticks(x, x)
+    plt.xlabel(str, fontsize=20)
+    plt.ylabel('difference', fontsize=20)
+    plt.legend()
+    plt.show()
+"""
 
 if __name__ == '__main__':
     main()
