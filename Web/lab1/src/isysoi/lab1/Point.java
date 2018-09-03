df5a4447ebf90cb2730c6aa05cc85daa3b1fe2b8
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-03 16:53:42 +0300

    web java: <<lab1>>: add final

--- Web/lab1/src/isysoi/lab1/Point.java
+++ Web/lab1/src/isysoi/lab1/Point.java
@@ -2,8 +2,8 @@ package isysoi.lab1;
 
 public class Point {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Point(int x, int y) {
         this.x = x;
