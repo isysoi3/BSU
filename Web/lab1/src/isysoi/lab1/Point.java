ce26ede95964629ddf12a283a5a5a4555592dc22
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-03 16:49:02 +0300

    web java: <<lab1>>: delete setters

--- Web/lab1/src/isysoi/lab1/Point.java
+++ Web/lab1/src/isysoi/lab1/Point.java
@@ -14,15 +14,7 @@ public class Point {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 }
