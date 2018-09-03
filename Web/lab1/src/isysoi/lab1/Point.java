d4c63baeee1fb796bf89fe578cbbfe98824dc008
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-03 17:20:29 +0300

    web java: <<lab1>>: points distance and toString overriding

--- Web/lab1/src/isysoi/lab1/Point.java
+++ Web/lab1/src/isysoi/lab1/Point.java
@@ -1,5 +1,7 @@
 package isysoi.lab1;
 
+import java.util.Arrays;
+
 public class Point {
 
     private final int x;
@@ -17,4 +19,26 @@ public class Point {
     public int getY() {
         return y;
     }
+
+    static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    /**
+     * Distance from this point to the supplied point.
+     *
+     * @param that the that
+     * @return the double
+     */
+    public double distance(Point that) {
+        return distance(this, that);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
 }
+
+
