23b0be494b4b2f5aa3e2c33656d25507571e6b4c
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-03 16:37:45 +0300

    web java: <<lab1>>: add setter and getter with constructor for Quadrilateral

--- Web/lab1/src/isysoi/lab1/Quadrilateral.java
+++ Web/lab1/src/isysoi/lab1/Quadrilateral.java
@@ -2,4 +2,22 @@ package isysoi.lab1;
 
 public class Quadrilateral {
 
+    private Point [] points = new Point[4];
+
+    public Quadrilateral(Point[] points) {
+        this.points = points;
+    }
+
+    public Quadrilateral() {
+
+    }
+
+    public Point[] getPoints() {
+        return points;
+    }
+
+    public void setPoints(Point[] points) {
+        this.points = points;
+    }
+
 }
