cc56f9411407ae0bad914c4418523337536adb30
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-04 16:42:23 +0300

    asp.net: <<lab1>>: init project

---
+++ ASP.NET/lab1/lab1/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
