3f0c492cfab53995456f6826629256fd4ea8bb69
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-05 18:00:29 +0300

    web java: <<lab1>>: refactor main class and replace to another package

---
+++ Web/lab1/src/View/Main.java
@@ -0,0 +1,20 @@
+package View;
+
+import Controller.Controller;
+
+/**
+ * Main class which run Controller's demonstrate method
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
+
+public class Main {
+
+    /**
+     * The Main method
+     * @param args command line parameters
+     */
+    public static void main(String[] args) {
+        Controller.startWork();
+    }
+}
