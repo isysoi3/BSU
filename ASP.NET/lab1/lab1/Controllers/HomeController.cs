efccb16bbc475812e75aae6185389e289326708a
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-10 22:06:17 +0300

    ASP.NET: <<lab1>>: some work done, i don`t hnow how it work

--- ASP.NET/lab1/lab1/Controllers/HomeController.cs
+++ ASP.NET/lab1/lab1/Controllers/HomeController.cs
@@ -20,32 +20,54 @@ namespace lab1.Controllers
         public IActionResult Table(string tablename, int fieldsnumber)
         {
             TableModel tableModel = new TableModel();
-            tableModel.name = tablename;
-            tableModel.numberOfFields = fieldsnumber;
+            tableModel.Name = tablename;
+            tableModel.NumberOfAttributes = fieldsnumber;
 
             return View(tableModel);
         }
 
         [HttpPost]
-        public IActionResult CreatedTable(TableModel tableModel)
+        public IActionResult CreatedTable(string tablename, int fieldsnumber)
         {
-            List<Field> listValues = new List<Field>();
+            TableModel tableModel = new TableModel();
+            tableModel.Name = tablename;
+            tableModel.NumberOfAttributes = fieldsnumber;
+
+            List<Models.Attribute> listValues = new List<Models.Attribute>();
             for (int i = 0; i < 3; i++)
             {
-                var field = new Field();
-                field.name = Request.Form[String.Format("{0} {1}", "fieldName", i)];
-                field.type = Request.Form[String.Format("{0} {1}", "fieldType", i)];
+                var field = new Models.Attribute();
+                field.Name = Request.Form[String.Format("{0} {1}", "fieldName", i)];
+                field.Type = Request.Form[String.Format("{0} {1}", "fieldType", i)];
                 listValues.Add(field);
 
 
             }
-            tableModel.fields = listValues;
+            tableModel.Attributes = listValues;
             return View(tableModel);
         }
 
         [HttpPost]
-        public IActionResult AddTableEntries(TableModel tableModel)
+        public IActionResult AddTableEntries(string tablename, 
+            int fieldsnumber,
+            List<Models.Attribute> attributes)
         {
+            TableModel tableModel = new TableModel();
+            tableModel.Name = tablename;
+            tableModel.NumberOfAttributes = fieldsnumber;
+
+            List<Models.Attribute> listValues = new List<Models.Attribute>();
+            for (int i = 0; i < 3; i++)
+            {
+                var field = new Models.Attribute();
+                field.Name = Request.Form[String.Format("{0} {1}", "fieldName", i)];
+                field.Type = Request.Form[String.Format("{0} {1}", "fieldType", i)];
+                listValues.Add(field);
+
+
+            }
+            tableModel.Attributes = listValues;
+
             return View(tableModel);
         }
 
