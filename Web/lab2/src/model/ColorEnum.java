6c058059c2b60e561738e8384c24788996a600f2
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-12 23:09:35 +0300

    web: <<lab2>>: move color enum to another file

---
+++ Web/lab2/src/model/ColorEnum.java
@@ -0,0 +1,19 @@
+package model;
+
+enum ColorEnum {
+    RED {
+        public String toString() {
+            return "red";
+        }
+    },
+    GREEN {
+        public String toString() {
+            return "green";
+        }
+    },
+    YELLOW {
+        public String toString() {
+            return "yellow";
+        }
+    }
+}
--- Web/lab2/src/model/Syrup.java
+++ Web/lab2/src/model/Syrup.java
@@ -10,24 +10,6 @@ public class Syrup extends Medicine {
         return color;
     }
 
-    enum ColorEnum {
-        RED {
-            public String toString() {
-                return "red";
-            }
-        },
-        GREEN {
-            public String toString() {
-                return "green";
-            }
-        },
-        YELLOW {
-            public String toString() {
-                return "yellow";
-            }
-        }
-    }
-
     public Syrup(String name, double price, Date manufactureDate, Date expirationDate, ColorEnum color) {
         super(name, price, manufactureDate, expirationDate);
         this.color = color;
