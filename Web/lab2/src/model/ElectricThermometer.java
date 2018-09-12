aebfeec03b28c4e641cf12d862d7e43e23e9c520
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-12 21:37:34 +0300

    web: <<lab2>> add Thermometr

---
+++ Web/lab2/src/model/ElectricThermometer.java
@@ -0,0 +1,28 @@
+package model;
+
+public class ElectricThermometer extends Medicine {
+
+    protected double temperature;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public void someWork() {
+
+    }
+
+    public void turnOn() {
+
+    }
+
+    public void turnOff() {
+
+    }
+
+    public void measureTemperature() {
+
+    }
+
+}
