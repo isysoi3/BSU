118e06dc9197738b9c8782831a5e17a1f608d52d
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-12 20:56:27 +0300

    web: <<lab2>> add antibiotic

---
+++ Web/lab2/src/model/Antibiotic.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Antibiotic extends Medicine {
+
+    protected Boolean isRecipeRequired;
+
+    public Boolean getRecipeRequired() {
+        return isRecipeRequired;
+    }
+
+    @Override
+    public void someWork() {
+
+    }
+
+}
