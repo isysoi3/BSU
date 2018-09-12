e551f8db3f941213aa9479b0beab40d7fb54f71d
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-12 22:02:27 +0300

    web: <<lab2>> add docs

--- Web/lab2/src/controller/Controller.java
+++ Web/lab2/src/controller/Controller.java
@@ -1,13 +1,17 @@
 package controller;
 
 /**
- * Basic class demonstrates functions of Quadrilateral and Point classes
+ * Basic class demonstrates functions of classes
  *
  * @author Ilya Sysoi
  * @version 1.0.0
  */
 public class Controller {
 
+    /**
+     * Method to demonstrate work with  classes
+     * @return some work result to show it on view
+     */
     public static String demonstrateWork() {
 
         return "";
