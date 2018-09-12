a3d54b49bf164443838e2d8d0c2dcfd42c33f765
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-12 20:37:47 +0300

    web: <<lab2>>: add Pill

---
+++ Web/lab2/src/model/Pill.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Pill extends Medicine {
+
+    private int doseInMg;
+
+    public int getDoseInMg() {
+        return doseInMg;
+    }
+
+    @Override
+    public void someWork() {
+
+    }
+}
