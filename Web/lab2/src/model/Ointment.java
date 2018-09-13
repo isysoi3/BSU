bc8900eb6dc8ecba2081ac21f25e98d74342d5e5
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-13 22:34:43 +0300

    web: <<lab2>>: add java docs

--- Web/lab2/src/model/Ointment.java
+++ Web/lab2/src/model/Ointment.java
@@ -2,18 +2,39 @@ package model;
 
 import java.util.Date;
 
+/**
+ * a smooth oily preparation that is rubbed on the skin for medicinal purposes
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
 public class Ointment extends Medicine {
 
-    private double fluidity;
+    /**
+     * percent of being fluid of ointment
+     */
+    private double fluidityPercent;
 
-    public double getFluidity() {
-        return fluidity;
+    /**
+     * getter of fluidity
+     * @return fluidity of ointment
+     */
+    public double getFluidityPercent() {
+        return fluidityPercent;
     }
 
 
-    public Ointment(String name, double price, Date manufactureDate, Date expirationDate, double fluidity) {
+    /**
+     * Constructor, creates medicine with name, price and dates
+     * @param name name of medicine
+     * @param price price of medicine
+     * @param manufactureDate date when it was produced
+     * @param expirationDate date when it wil expire
+     * @param fluidityPercent percent of being fluidK
+     * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
+     */
+    public Ointment(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent) {
         super(name, price, manufactureDate, expirationDate);
-        this.fluidity = fluidity;
+        this.fluidityPercent = fluidityPercent;
     }
 
     @Override
