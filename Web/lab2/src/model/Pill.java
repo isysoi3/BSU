c3ab55e70b5ff792e416ca2ed6395f142ea0a4cf
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-13 22:28:48 +0300

    web: <<lab2>>: add java docs

--- Web/lab2/src/model/Pill.java
+++ Web/lab2/src/model/Pill.java
@@ -2,14 +2,35 @@ package model;
 
 import java.util.Date;
 
+/**
+ * a small round mass of solid medicine to be swallowed whole.
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
 public class Pill extends Medicine {
 
+    /**
+     * the weight of active additives
+     */
     private int doseInMg;
 
+    /**
+     * getter of dose in pill
+     * @return weight of active additives
+     */
     public int getDoseInMg() {
         return doseInMg;
     }
 
+    /**
+     * Constructor, creates medicine with name, price and dates
+     * @param name name of medicine
+     * @param price price of medicine
+     * @param manufactureDate date when it was produced
+     * @param expirationDate date when it wil expire
+     * @param doseInMg weight of active additives
+     * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
+     */
     public Pill(String name, double price, Date manufactureDate, Date expirationDate, int doseInMg) {
         super(name, price, manufactureDate, expirationDate);
         this.doseInMg = doseInMg;
