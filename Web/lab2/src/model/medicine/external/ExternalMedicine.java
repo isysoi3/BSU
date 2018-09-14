4c0bffaff7ed7b1ca616373ade4b88510a5ba967
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-14 13:42:30 +0300

    web: <<lab2>>: move logic to base class

--- Web/lab2/src/model/medicine/external/ExternalMedicine.java
+++ Web/lab2/src/model/medicine/external/ExternalMedicine.java
@@ -6,8 +6,48 @@ import java.util.Date;
 
 public class ExternalMedicine extends Medicine {
 
-    public ExternalMedicine(String name, double price, Date manufactureDate, Date expirationDate) {
+    /**
+     * percent of being fluid of ointment
+     */
+    private double fluidityPercent;
+
+    /**
+     * getter of fluidity
+     * @return fluidity of ointment
+     */
+    public double getFluidityPercent() {
+        return fluidityPercent;
+    }
+
+
+    public ExternalMedicine(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent) {
         super(name, price, manufactureDate, expirationDate);
+        this.fluidityPercent = fluidityPercent;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Medicine medicine = (Medicine) obj;
+        ExternalMedicine externalMedicine = (ExternalMedicine) obj;
+
+        return (externalMedicine.fluidityPercent == this.fluidityPercent)
+                && medicine.equals(this);
+    }
+
+    @Override
+    public String toString() {
+        String stringFromSuper = super.toString();
+        return String.format("%s Fluidity percent: %f\n", stringFromSuper, fluidityPercent);
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCodeFromSuper = super.hashCode();
+        return hashCodeFromSuper + (int)fluidityPercent * 31;
     }
 
     @Override
--- Web/lab2/src/model/medicine/external/Ointment.java
+++ Web/lab2/src/model/medicine/external/Ointment.java
@@ -12,20 +12,6 @@ import java.util.Date;
 public class Ointment extends ExternalMedicine {
 
     /**
-     * percent of being fluid of ointment
-     */
-    private double fluidityPercent;
-
-    /**
-     * getter of fluidity
-     * @return fluidity of ointment
-     */
-    public double getFluidityPercent() {
-        return fluidityPercent;
-    }
-
-
-    /**
      * Constructor, creates medicine with name, price and dates
      * @param name name of medicine
      * @param price price of medicine
@@ -35,32 +21,7 @@ public class Ointment extends ExternalMedicine {
      * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
      */
     public Ointment(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent) {
-        super(name, price, manufactureDate, expirationDate);
-        this.fluidityPercent = fluidityPercent;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Medicine medicine = (Medicine) obj;
-        Ointment ointment = (Ointment) obj;
-
-        return (ointment.fluidityPercent == this.fluidityPercent)
-                && medicine.equals(this);
-    }
-
-    @Override
-    public String toString() {
-        String stringFromSuper = super.toString();
-        return String.format("%s Fluidity percent: %f\n", stringFromSuper, fluidityPercent);
-    }
-
-    @Override
-    public int hashCode() {
-        int hashCodeFromSuper = super.hashCode();
-        return hashCodeFromSuper + (int)fluidityPercent * 31;
+        super(name, price, manufactureDate, expirationDate, fluidityPercent);
     }
 
     @Override
