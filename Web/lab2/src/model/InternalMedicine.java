6906136b82faf330ee128b00686ee9d7b5c18cf2
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-14 13:17:57 +0300

    web: <<lab2>>: add external and internak

---
+++ Web/lab2/src/model/InternalMedicine.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class InternalMedicine extends Medicine {
+
+    public InternalMedicine(String name, double price, Date manufactureDate, Date expirationDate) {
+        super(name, price, manufactureDate, expirationDate);
+    }
+
+    @Override
+    public void effect() {
+
+    }
+
+}
