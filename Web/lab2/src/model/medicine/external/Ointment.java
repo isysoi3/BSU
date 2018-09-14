423e66789860988938c6e3939c5faa65a70046a1
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-14 13:47:54 +0300

    web: <<lab2>>: add some logic

--- Web/lab2/src/model/medicine/external/Ointment.java
+++ Web/lab2/src/model/medicine/external/Ointment.java
@@ -11,6 +11,9 @@ import java.util.Date;
  */
 public class Ointment extends ExternalMedicine {
 
+    private boolean tmp;
+
+
     /**
      * Constructor, creates medicine with name, price and dates
      * @param name name of medicine
@@ -20,12 +23,38 @@ public class Ointment extends ExternalMedicine {
      * @param fluidityPercent percent of being fluidK
      * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
      */
-    public Ointment(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent) {
+    public Ointment(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent, boolean tmp) {
         super(name, price, manufactureDate, expirationDate, fluidityPercent);
+        this.tmp = tmp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        ExternalMedicine externalMedicine = (ExternalMedicine) obj;
+        Ointment ointment = (Ointment) obj;
+
+        return (ointment.tmp == this.tmp)
+                && externalMedicine.equals(this);
+    }
+
+    @Override
+    public String toString() {
+        String stringFromSuper = super.toString();
+        return String.format("%s Tmp: %b\n", stringFromSuper, tmp);
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCodeFromSuper = super.hashCode();
+        return hashCodeFromSuper + (tmp ? 2 : -2 ) * 13;
     }
 
     @Override
     public void effect() {
 
     }
+
 }
