67dee3a2013c982fdb1d6dc37a38480ae1ea4770
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-15 16:48:27 +0300

    web: <<lab2>>: add java doc

--- Web/lab2/src/model/medicine/internal/Pill.java
+++ Web/lab2/src/model/medicine/internal/Pill.java
@@ -1,11 +1,10 @@
 package model.medicine.internal;
 
-import model.medicine.Medicine;
-
 import java.util.Date;
 
 /**
  * a small round mass of solid medicine to be swallowed whole.
+ *
  * @author Ilya Sysoi
  * @version 1.0.0
  */
@@ -18,6 +17,7 @@ public class Pill extends InternalMedicine {
 
     /**
      * getter of dose in pill
+     *
      * @return weight of active additives
      */
     public int getDoseInMg() {
@@ -26,12 +26,13 @@ public class Pill extends InternalMedicine {
 
     /**
      * Constructor, creates medicine with name, price and dates
-     * @param name name of medicine
-     * @param price price of medicine
-     * @param manufactureDate date when it was produced
-     * @param expirationDate date when it wil expire
-     * @param doseInMg weight of active additives
-     * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
+     *
+     * @param name             name of medicine
+     * @param price            price of medicine
+     * @param manufactureDate  date when it was produced
+     * @param expirationDate   date when it wil expire
+     * @param maxDailyDoseInMg maximum daily dose that can be accepted
+     * @param doseInMg         weight of active additives
      */
     public Pill(String name, double price, Date manufactureDate, Date expirationDate, double maxDailyDoseInMg, int doseInMg) {
         super(name, price, manufactureDate, expirationDate, maxDailyDoseInMg);
