071667ab3267b3083090cb1bf4ab937cf97030c8
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-15 16:25:13 +0300

    web: <<lab2>>: add java doc

--- Web/lab2/src/model/medicine/external/Gell.java
+++ Web/lab2/src/model/medicine/external/Gell.java
@@ -2,21 +2,35 @@ package model.medicine.external;
 
 import java.util.Date;
 
+/**
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
 public class Gell extends ExternalMedicine {
 
+    /**
+     * cooling effect of gell
+     */
     private boolean isСooling;
 
+    /**
+     * getter of cooling effect
+     *
+     * @return is cooling
+     */
     public boolean isСooling() {
         return isСooling;
     }
 
     /**
      * Constructor, creates medicine with name, price and dates
-     * @param name name of medicine
-     * @param price price of medicine
+     *
+     * @param name            name of medicine
+     * @param price           price of medicine
      * @param manufactureDate date when it was produced
-     * @param expirationDate date when it wil expire
-     * @param fluidityPercent percent of being fluidK
+     * @param expirationDate  date when it wil expire
+     * @param fluidityPercent percent of being fluid
+     * @param isСooling       effect of gell that cool
      * @throws IllegalArgumentException if price is bellow 0 or manufactureDate after expirationDate
      */
     public Gell(String name, double price, Date manufactureDate, Date expirationDate, double fluidityPercent, boolean isСooling) {
@@ -45,7 +59,7 @@ public class Gell extends ExternalMedicine {
     @Override
     public int hashCode() {
         int hashCodeFromSuper = super.hashCode();
-        return hashCodeFromSuper + (isСooling ? 1 : -1 ) * 31;
+        return hashCodeFromSuper + (isСooling ? 1 : -1) * 31;
     }
 
     @Override
