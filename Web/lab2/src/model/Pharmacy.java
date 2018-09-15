ca5c63d2718ca6d081d18c0f93d8225cd3ab055f
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-15 14:10:18 +0300

    web: <<lab2>>: adding medicines to array and count they cost

--- Web/lab2/src/model/Pharmacy.java
+++ Web/lab2/src/model/Pharmacy.java
@@ -1,7 +1,9 @@
 package model;
 
 import model.medicine.Medicine;
+import model.medicine.builder.MedicineBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,7 +20,56 @@ public class Pharmacy {
     }
 
     public Pharmacy() {
+        MedicineDirector director = new MedicineDirector();
+        MedicineBuilder builder = new MedicineBuilder();
 
+        medicines = new ArrayList<>();
+
+        medicines.add(director.manufactureAntibiotic(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureGell(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureGell(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureOintment(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureAntibiotic(builder));
+        builder.reset();
+
+        medicines.add(director.manufacturePill(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureSyrup(builder));
+        builder.reset();
+
+        medicines.add(director.manufacturePill(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureGell(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureOintment(builder));
+        builder.reset();
+
+        medicines.add(director.manufacturePill(builder));
+        builder.reset();
+
+        medicines.add(director.manufactureAntibiotic(builder));
+        builder.reset();
+
+    }
+
+    public double countTotalPrice() {
+        double totalPrice = 0;
+        for (Medicine medicine:
+             medicines) {
+            totalPrice += medicine.getPrice();
+        }
+        return totalPrice;
     }
 
 }
