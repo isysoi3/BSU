3c7946b8dab394d0c45abb9eddbe885bb32e9637
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-15 13:43:59 +0300

    web: <<lab2>>: add list of medicine

--- Web/lab2/src/model/Pharmacy.java
+++ Web/lab2/src/model/Pharmacy.java
@@ -1,9 +1,24 @@
 package model;
 
+import model.medicine.Medicine;
+
+import java.util.List;
+
 /**
  * this is pharmacy when store and sell medicines
  * @author Ilya Sysoi
  * @version 1.0.0
  */
 public class Pharmacy {
+
+    private List<Medicine> medicines;
+
+    public List<Medicine> getMedicines() {
+        return medicines;
+    }
+
+    public Pharmacy() {
+
+    }
+
 }
