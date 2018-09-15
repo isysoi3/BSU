03acc78512c4d190937838b247ba7653a76ad789
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-15 13:44:19 +0300

    web: <<lab2>>: add interface builder

---
+++ Web/lab2/src/model/Builder.java
@@ -0,0 +1,33 @@
+package model;
+
+import model.medicine.internal.ColorEnum;
+
+import java.util.Date;
+
+public interface Builder {
+
+    void setName(String name);
+
+    void setPrice(double price);
+
+    void setManufactureDate(Date date);
+
+    void setExpirationDate(Date date);
+
+    void setFluidityPercent(double percent);
+
+    void setMaxDailyDoseInMg(double dose);
+
+    void isCooling(boolean isCooling);
+
+    void isWarming(boolean isWarming);
+
+    void setDoseInMg(int dose);
+
+    void setColor(ColorEnum color);
+
+    void setRecipeRequired(boolean isRecipeRequired);
+
+    void reset();
+
+}
