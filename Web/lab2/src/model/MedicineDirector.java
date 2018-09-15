5b3b37921a837142d67442f34edd30310597c848
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-15 15:34:06 +0300

    web: <<lab2>>: add java doc, rename method

--- Web/lab2/src/model/MedicineDirector.java
+++ Web/lab2/src/model/MedicineDirector.java
@@ -7,12 +7,22 @@ import model.medicine.internal.ColorEnum;
 import java.util.Date;
 import java.util.Random;
 
-//TODO: create one instance of random
+/**
+ * director that help builder to build medicine items
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
 public class MedicineDirector {
 
+    private Random random = new Random();
+
+    /**
+     * manufacture pill
+     * @return configured pill
+     */
     public Medicine manufacturePill(Builder builder) {
         builder.setName("pill");
-        builder.setPrice((new Random()).nextDouble() % 10 + 1);
+        builder.setPrice(random.nextDouble() % 10 + 1);
         builder.setManufactureDate(new Date());
         builder.setExpirationDate(new Date());
         builder.setMaxDailyDoseInMg(12);
@@ -20,10 +30,13 @@ public class MedicineDirector {
         return builder.getResult();
     }
 
-
+    /**
+     * manufacture antibiotic
+     * @return configured antibiotic
+     */
     public Medicine manufactureAntibiotic(Builder builder) {
         builder.setName("antibiotic");
-        builder.setPrice((new Random()).nextDouble() % 10 + 1);
+        builder.setPrice(random.nextDouble() % 10 + 1);
         builder.setManufactureDate(new Date());
         builder.setExpirationDate(new Date());
         builder.setMaxDailyDoseInMg(12);
@@ -31,10 +44,13 @@ public class MedicineDirector {
         return builder.getResult();
     }
 
-
+    /**
+     * manufacture syrup
+     * @return configured syrup
+     */
     public Medicine manufactureSyrup(Builder builder) {
         builder.setName("syrup");
-        builder.setPrice((new Random()).nextDouble() % 10 + 1);
+        builder.setPrice(random.nextDouble() % 10 + 1);
         builder.setManufactureDate(new Date());
         builder.setExpirationDate(new Date());
         builder.setMaxDailyDoseInMg(12);
@@ -42,10 +58,13 @@ public class MedicineDirector {
         return builder.getResult();
     }
 
-
+    /**
+     * manufacture gell
+     * @return configured gell
+     */
     public Medicine manufactureGell(Builder builder) {
         builder.setName("gell");
-        builder.setPrice((new Random()).nextDouble() % 10 + 1);
+        builder.setPrice(random.nextDouble() % 10 + 1);
         builder.setManufactureDate(new Date());
         builder.setExpirationDate(new Date());
         builder.setFluidityPercent(12.4);
@@ -53,9 +72,13 @@ public class MedicineDirector {
         return builder.getResult();
     }
 
+    /**
+     * manufacture ointment
+     * @return configured ointment
+     */
     public Medicine manufactureOintment(Builder builder) {
         builder.setName("ointment");
-        builder.setPrice((new Random()).nextDouble() % 10 + 1);
+        builder.setPrice(random.nextDouble() % 10 + 1);
         builder.setManufactureDate(new Date());
         builder.setExpirationDate(new Date());
         builder.setFluidityPercent(50.4);
