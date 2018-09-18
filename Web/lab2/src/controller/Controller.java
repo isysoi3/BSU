0b1fe76d653cd024359721e1764da091e9521f4a
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-18 15:38:36 +0300

    web: <<lab2>>: add demonstration of sorting methods

--- Web/lab2/src/controller/Controller.java
+++ Web/lab2/src/controller/Controller.java
@@ -25,4 +25,56 @@ public class Controller {
         return medicines;
     }
 
+    /**
+     * Method to demonstrate work of sorting method by name
+     *
+     * @param isRevesed determinate order of sorted list
+     * @return some work result to show it on view
+     */
+    public static List<Medicine> getPharmacyMedicinesSortedByName(boolean isRevesed) {
+        var pharmacy = new Pharmacy();
+        var sortedMedicines = pharmacy.sortMedicinesByName(isRevesed);
+
+        return sortedMedicines;
+    }
+
+    /**
+     * Method to demonstrate work of sorting method by price
+     *
+     * @param isRevesed determinate order of sorted list
+     * @return some work result to show it on view
+     */
+    public static List<Medicine> getPharmacyMedicinesSortedByPrice(boolean isRevesed) {
+        var pharmacy = new Pharmacy();
+        var sortedMedicines = pharmacy.sortMedicinesByPrice(isRevesed);
+
+        return sortedMedicines;
+    }
+
+    /**
+     * Method to demonstrate work of sorting method by manufacture date
+     *
+     * @param isRevesed determinate order of sorted list
+     * @return some work result to show it on view
+     */
+    public static List<Medicine> getPharmacyMedicinesSortedByManufactureDate(boolean isRevesed) {
+        var pharmacy = new Pharmacy();
+        var sortedMedicines = pharmacy.sortMedicinesByManufactureDate(isRevesed);
+
+        return sortedMedicines;
+    }
+
+    /**
+     * Method to demonstrate work of sorting method by expiration date
+     *
+     * @param isRevesed determinate order of sorted list
+     * @return some work result to show it on view
+     */
+    public static List<Medicine> getPharmacyMedicinesSortedByExpirationDate(boolean isRevesed) {
+        var pharmacy = new Pharmacy();
+        var sortedMedicines = pharmacy.sortMedicinesByExpirationDate(isRevesed);
+
+        return sortedMedicines;
+    }
+
 }
