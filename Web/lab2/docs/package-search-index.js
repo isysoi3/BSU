bf23f343c7d3cf47998c84cc941aa8fdb9931d0b
Ilya Sysoi <14ilya12322@gmil.com>
2018-09-22 11:03:15 +0300

    web: <<lab2>>: generate java doc

--- Web/lab2/docs/package-search-index.js
+++ Web/lab2/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"controller"},{"l":"model"},{"l":"model.medicine"},{"l":"model.medicine.builder"},{"l":"model.medicine.external"},{"l":"model.medicine.internal"},{"l":"view"}]
+packageSearchIndex = [{"l":"controller"},{"l":"controller.builder"},{"l":"controller.director"},{"l":"model"},{"l":"model.medicine"},{"l":"model.medicine.comparator"},{"l":"model.medicine.external"},{"l":"model.medicine.internal"},{"l":"view"}]
