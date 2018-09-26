27bc899f6339e022ac89b0fb39395d5565aa9e72
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-26 22:17:20 +0300

    web: <<lab2_2>>: update gui

--- Web/lab2_FX/src/sample/Main.java
+++ Web/lab2_FX/src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Pharmacey MINSK 2.0");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
