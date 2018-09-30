dc001d99bf518a3fbf9ed235b4dc3af482fa31c0
Ilya Sysoi <14ilya12322@gmail.com>
2018-09-30 16:13:46 +0300

    web: <<lab2_2>>: done?

--- Web/lab2_FX/src/controller/Controller.java
+++ Web/lab2_FX/src/controller/Controller.java
@@ -1,11 +1,12 @@
 package controller;
 
+import controller.builder.MedicineBuilder;
+import controller.director.MedicineDirector;
 import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import model.medicine.Medicine;
 import model.pharmacy.Pharmacy;
 import model.pharmacy.PharmacyManager;
@@ -15,6 +16,23 @@ import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 
 public class Controller {
+
+    /**
+     * selected name text field
+     */
+    @FXML
+    public TextField selectNameTextField;
+
+    /**
+     * selected min price text field
+     */
+    @FXML
+    public TextField selectMinPriceTextField;
+    /**
+     * selected max price text field
+     */
+    @FXML
+    public TextField selectMaxPriceTextField;
 
     /**
      * table view of medicines
@@ -53,21 +71,54 @@ public class Controller {
     private TextArea medicineDescriptionTextArea;
 
 
+    private Pharmacy pharmacy = new Pharmacy(new PharmacyManager());
+
     /**
      * initialize some UI method
      */
     @FXML
     private void initialize() {
+        addTestData();
+        configureTableViewWithColumns();
+    }
+
+    /**
+     * test medicine data to pharmacy
+     */
+    private void addTestData() {
+        MedicineDirector director = new MedicineDirector();
+        MedicineBuilder builder = new MedicineBuilder();
+        pharmacy.addMedicine(director.manufactureAntibiotic(builder, "Antibiotic4", 5.5, false));
+        builder.reset();
+
+        pharmacy.addMedicine(director.manufactureGell(builder, "gel1", 12.4, true));
+        builder.reset();
+
+        pharmacy.addMedicine(director.manufactureGell(builder, "gel2", 32.4, false));
+        builder.reset();
+
+        pharmacy.addMedicine(director.manufactureOintment(builder, "Ointment1", 50.5, true));
+        builder.reset();
+
+        pharmacy.addMedicine(director.manufactureAntibiotic(builder, "Antibiotic2", 50.5, true));
+        builder.reset();
+    }
+
+    /**
+     * configure table view with columns
+     */
+    private void configureTableViewWithColumns() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         nameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
         priceColumn.setCellValueFactory(cellData ->
-                new ReadOnlyDoubleWrapper(BigDecimal.valueOf(cellData.getValue().getPrice())
-                        .setScale(3, RoundingMode.HALF_UP)
-                        .doubleValue()));
+                new ReadOnlyDoubleWrapper(
+                        BigDecimal.valueOf(cellData.getValue().getPrice())
+                                .setScale(3, RoundingMode.HALF_DOWN)
+                                .doubleValue()));
         manufactureDateColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(dateFormat.format(cellData.getValue().getManufactureDate())));
         expirationDateColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(dateFormat.format(cellData.getValue().getExpirationDate())));
 
-        var medicines = (new PharmacyController(new Pharmacy(new PharmacyManager()))).getPharmacyMedicines();
+        var medicines = pharmacy.getMedicines();
         tableView.getItems().addAll(medicines);
 
         tableView.setEditable(false);
@@ -80,5 +131,54 @@ public class Controller {
         });
     }
 
+    /**
+     * handling tap select by name button
+     */
+    public void onSelectByNameTapped(ActionEvent actionEvent) {
+        var name = selectNameTextField.getText();
+        if (name.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Enter name", ButtonType.OK);
+            alert.showAndWait();
 
+            if (alert.getResult() == ButtonType.OK) {
+                alert.hide();
+            }
+        } else {
+            tableView.getItems().clear();
+            tableView.getItems().addAll(pharmacy.selectMedicinesByName(name));
+        }
+    }
+
+    /**
+     * handling tap drlrvt by price button
+     */
+    public void onSelectByPriceTapped(ActionEvent actionEvent) {
+        var minPrice = selectMinPriceTextField.getText();
+        var maxPrice = selectMaxPriceTextField.getText();
+        if (maxPrice.isEmpty() || minPrice.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Enter price range", ButtonType.OK);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.OK) {
+                alert.hide();
+            }
+        } else {
+            System.out.println("Min" + minPrice);
+            System.out.println("Max" + maxPrice);
+
+            tableView.getItems().clear();
+            tableView.getItems().addAll(pharmacy.selectMedicinesByPriceRange(Double.valueOf(minPrice), Double.valueOf(maxPrice)));
+        }
+    }
+
+    /**
+     * handling tap reset button
+     */
+    public void onResetButtonTapped(ActionEvent actionEvent) {
+        tableView.getItems().clear();
+        selectNameTextField.setText(null);
+        selectMinPriceTextField.setText(null);
+        selectMaxPriceTextField.setText(null);
+        tableView.getItems().addAll(pharmacy.getMedicines());
+    }
 }
--- Web/lab2_FX/src/model/pharmacy/Pharmacy.java
+++ Web/lab2_FX/src/model/pharmacy/Pharmacy.java
@@ -1,5 +1,7 @@
 package model.pharmacy;
 
+import controller.builder.MedicineBuilder;
+import controller.director.MedicineDirector;
 import model.medicine.Medicine;
 
 import java.util.ArrayList;
