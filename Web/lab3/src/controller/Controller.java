eee949c69e94c81f64060b4b2f05ae4d37b63de5
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-06 18:31:45 +0300

    web: <<lab3>>: add paragraph

--- Web/lab3/src/controller/Controller.java
+++ Web/lab3/src/controller/Controller.java
@@ -1,17 +1,12 @@
 package controller;
 
 import localization.LocaleWrapper;
-import model.text_units.Text;
-import model.text_units.TextPart;
-import model.text_units.Word;
-import model.util.Splitter;
-import model.util.VowelComparator;
+import model.Text;
+import model1231.util.Splitter;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,8 +14,14 @@ import java.util.regex.Pattern;
  * Created by lenovo on 17.10.2017.
  */
 public class Controller {
-    Text text;
-    VowelComparator wc;
+
+    private Text text;
+
+    public Text getText(){
+        return text;
+    }
+
+
     public String loadController(String path, String fileLocale, String programLocale){
         try {
             String text = new String( Files.readAllBytes( Paths.get( path ) ) );
@@ -41,38 +42,5 @@ public class Controller {
         return res+"\n\n";
    }
 
-    public String findMaxSubStr(){
-        Pattern pattern = Pattern.compile("[^a-zA-Z\\u0430-\\u044F\\u0410-\\u042F]+");
-        Matcher matcher = pattern.matcher(text.getValue());
-        int max_length = 0;
-        String max_str = "";
-        while (matcher.find()) {
-            String str = matcher.group();
-            if (str.length() > max_length){
-                max_length = str.length();
-                max_str = str;
-            }
-        }
-        max_str = max_str.replaceAll("[\\n]", "\\\\n");
-        max_str = max_str.replaceAll("[\\r]", "\\\\r");
-        max_str = max_str.replaceAll("[\"]", "\\\\\"");
-        return LocaleWrapper.getLocalizedString(LocaleWrapper.MAX_SUB_STR) + " \"" + max_str + "\"";
-    }
 
-    public String sortByVolwes(List<TextPart> ltp){
-        ArrayList<Word> slw= new ArrayList<>();
-        for (TextPart tp: ltp)
-            if (tp instanceof Word)
-                slw.add((Word)tp);
-        String res = LocaleWrapper.getLocalizedString(LocaleWrapper.SORTED_WORDS) +
-                 ":\n____________________________________________________________________\n";
-        slw.sort(wc);
-        for (Word w: slw)
-            res+=w.getValue() + "\n";
-        return res+"\n___________________________________________________________________\n";
-    }
-
-    public Text getText(){
-        return text;
-    }
 }
