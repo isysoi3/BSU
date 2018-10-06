75b19a938c67b8ea9f93cc2e1a659390b05e5f71
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-06 18:32:21 +0300

    web: <<lab3>>: add text

---
+++ Web/lab3/src/model/Text.java
@@ -0,0 +1,43 @@
+package model;
+
+import model.text_unit.code.Code;
+import model.text_unit.text.TextUnit;
+import model.text_unit.text.TextUnitTypeEnum;
+import model.text_unit.text.part.Paragraph;
+import model.text_unit.text.part.Sentence;
+
+import java.util.ArrayList;
+
+public class Text extends TextUnit {
+
+    public static final String TEXT_NAME = "TEXT";
+    private ArrayList<TextUnit> text;
+
+
+    public Text() {
+        super(TextUnitTypeEnum.TEXT);
+        text = new ArrayList<>();
+    }
+
+    public void addSentence(Sentence sentence) {
+        text.add(sentence);
+    }
+
+    public void addParagraph(Paragraph paragraph) {
+        text.add(paragraph);
+    }
+
+    public void addCode(Code code) {
+        //text.add(code);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder textToString = new StringBuilder();
+        for (TextUnit textUnit : text) {
+            textToString.append(textUnit.toString());
+        }
+        return textToString.toString();
+    }
+
+}
