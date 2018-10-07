4e474fd54165cf58754aeef2936320c6fe80ded7
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-07 19:52:46 +0300

    web: <<lab3>>: add colors to console

--- Web/lab3/src/view/Main.java
+++ Web/lab3/src/view/Main.java
@@ -9,20 +9,24 @@ import static localization.LocaleWrapper.SWAP_FIRST_AND_LAST_WORDS;
 
 public class Main {
 
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         try {
             Controller controller = new Controller();
             String result = controller.loadText(args[0]);
-            System.out.println(result);
+            System.out.println(ANSI_GREEN + "------" + result + "------" + ANSI_RESET);
             System.out.println(controller.getTextString());
             System.out.println();
             System.out.println();
 
-            System.out.print((LocaleWrapper.getLocalizedString(START_COMBINE)));
+            System.out.print(ANSI_GREEN + "------" + (LocaleWrapper.getLocalizedString(START_COMBINE)) + "------" + ANSI_RESET);
             Text text = controller.parseTextStringToText();
             System.out.println(text.toString());
+            System.out.println();
 
-            System.out.println((LocaleWrapper.getLocalizedString(SWAP_FIRST_AND_LAST_WORDS)));
+            System.out.print(ANSI_GREEN + "------" + (LocaleWrapper.getLocalizedString(SWAP_FIRST_AND_LAST_WORDS)) + "------" + ANSI_RESET);
             System.out.println(controller.swapFirstAndLastWordInText(text).toString());
         } catch (Exception ex) {
             System.out.println(ex.toString());
