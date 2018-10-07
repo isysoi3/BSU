b2dba6cc5b36cc5bc28c019446ffb7458f84a7d9
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-07 13:02:59 +0300

    web: <<lab3>>: remove line

--- Web/lab3/src/model/text_unit/text/part/Word.java
+++ Web/lab3/src/model/text_unit/text/part/Word.java
@@ -7,7 +7,6 @@ public class Word extends TextUnit {
 
     public static final String WORD_NAME = "WORD";
     public static final String DIVIDER = ",:;'\"";
-    public static final String DELIM_FOR_COMBINING = " ";
     public static final String PATTERN = "[^ " + Paragraph.DIVIDER + Sentence.DIVIDER + DIVIDER +  "]+";
     private String text;
 
