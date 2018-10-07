420f3fa13c9e5a6fd1a163e64812cbbc13e8f6d1
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-07 13:30:44 +0300

    web: <<lab3>>: move logic to controller

--- Web/lab3/src/controller/Controller.java
+++ Web/lab3/src/controller/Controller.java
@@ -2,43 +2,37 @@ package controller;
 
 import localization.LocaleWrapper;
 import model.Text;
+import splitters.TextSplitter;
 
 import java.io.IOException;
+import java.lang.module.InvalidModuleDescriptorException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by lenovo on 17.10.2017.
- */
 public class Controller {
 
-    private Text text;
+    private String textString;
 
-    public Text getText(){
-        return text;
+    public String getTextString() {
+        return textString;
     }
 
-
-    public String loadText(String path, String fileLocale, String programLocale){
+    public String loadText(String path){
         try {
             byte [] encoded = Files.readAllBytes(Paths.get(path));
-            String text = new String(encoded, Charset.forName("windows-1251"));
-            return text;
-            //LocaleWrapper.setLocale(programLocale);
+            textString = new String(encoded, Charset.forName("windows-1251"));
+            return LocaleWrapper.getLocalizedString(LocaleWrapper.FILE_LOADED);
         } catch (IOException e) {
-            return LocaleWrapper.getLocalizedString(LocaleWrapper.INVALID_ARGS);
+            throw new IllegalArgumentException(LocaleWrapper.getLocalizedString(LocaleWrapper.INVALID_ARGS));
         }
-        //return LocaleWrapper.getLocalizedString(LocaleWrapper.FILE_LOADED);
     }
 
-   public String printTextPart(){
-        return text.toString();
-   }
-
-    public void parseTextStringToText(String text) {
+    public Text parseTextStringToText() {
+        TextSplitter textSplitter = new TextSplitter();
+        return textSplitter.splitText(textString);
 
     }
 
