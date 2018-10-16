aa302e4158a4aeffa35e63b06c19261ff63d3768
Ilya Sysoi <14ilya12322@gmil.com>
2018-10-16 15:37:17 +0300

    database: <<lab3>>: add task 3_2

--- Database/lab3/lab3.sql
+++ Database/lab3/lab3.sql
@@ -123,19 +123,76 @@ GROUP BY e.empname;
 --5: Найти сведения о номерах сотрудников, получивших зарплату за какой-либо месяц большую, чем средние зарплаты за все годы начислений.
 
 --6: Определить годы, в которые начисленная средняя зарплата была больше средней зарплаты за все годы начислений.
+SELECT year
+FROM SALARY
+GROUP BY year
+HAVING(AVG(salvalue)) > (SELECT AVG(salvalue) FROM salary)
 
 --7: Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты. 
+SELECT deptno 
+FROM DEPT d 
+WHERE deptno IN ( 
+	SELECT deptno 
+	FROM CAREER c
+	NATURAL JOIN EMP e
+	NATURAL JOIN SALARY s
+	WHERE s.salvalue IS NOT NULL)
+GROUP BY deptno;
 
 --8: Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
+SELECT deptno 
+FROM DEPT d 
+WHERE EXISTS
+	(SELECT deptno FROM CAREER c
+	NATURAL JOIN EMP e
+	NATURAL JOIN SALARY s
+	WHERE d.deptno = c.deptno);
 
 --9: Определить номера отделов, для сотрудников которых не начислялась зарплата.
+SELECT deptno 
+FROM DEPT d 
+WHERE NOT EXISTS
+	(SELECT deptno FROM CAREER c
+	NATURAL JOIN EMP e
+	NATURAL JOIN SALARY s
+	WHERE d.deptno = c.deptno);
 
 --10: Вывести сведения о карьере сотрудников с указанием названий и адресов отделов вместо номеров отделов.
+SELECT e.empname, d.deptname, d.deptaddr, c.startdate, c.enddate
+FROM EMP e
+NATURAL JOIN CAREER c
+NATURAL JOIN DEPT d
 
 --11: Определить целую часть средних зарплат,  по годам начисления.
+SELECT year, CAST(AVG(salvalue) as int) as avg_int_salvalue
+FROM SALARY 
+GROUP BY year
 
 --12: Разделите сотрудников на возрастные группы: A) возраст 20-30 лет; B) 31-40 лет; C) 41-50;    D) 51-60 или возраст не определён.
+SELECT empname,
+CASE 
+	WHEN MONTHS_BETWEEN(SYSDATE, birthdate) / 12 >= 20 and MONTHS_BETWEEN(SYSDATE, birthdate) / 12 <= 30 
+		THEN 'ГРУППА А'
+	WHEN MONTHS_BETWEEN(SYSDATE, birthdate) / 12 >= 31 and MONTHS_BETWEEN(SYSDATE, birthdate) / 12 <= 40
+		THEN 'ГРУППА B'
+	WHEN MONTHS_BETWEEN(SYSDATE, birthdate) / 12 >= 41 and MONTHS_BETWEEN(SYSDATE, birthdate) / 12 <= 50 
+		THEN 'ГРУППА С'
+	WHEN MONTHS_BETWEEN(SYSDATE, birthdate) / 12 >= 51 and MONTHS_BETWEEN(SYSDATE, birthdate) / 12 <= 60 
+		THEN 'ГРУППA D'
+	ELSE 'ГРУППА НЕ ОПРЕДЕЛЕНА' 
+END AS AGE_GROUP
+FROM EMP;
 
 --13: Перекодируйте номера отделов, добавив перед номером отдела буквы BI для номеров <=20,  буквы  LN для номеров >=30.
+SELECT 
+CASE 
+	WHEN deptno <= 20 
+		THEN CONCAT('BI', CAST(deptno as VARCHAR(10)))
+	WHEN deptno >= 30 
+		THEN CONCAT('LN', CAST(deptno as VARCHAR(10)))
+END AS deptno_with_prefix
+FROM DEPT;
 
 --14: Выдать информацию о сотрудниках из таблицы EMP, заменив отсутствие данного о дате рождения  датой '01-01-1000'.
+SELECT empname, empno, COALESCE(birthdate, TO_DATE('01-01-1000', 'dd-mm-yyyy')) AS BIRTHDATE 
+FROM EMP;
