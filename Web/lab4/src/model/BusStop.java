b91c50ec34b88c5deae28d02eead6023844aa308
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-28 15:29:53 +0300

    web: <<lab4>>: add bus stop and bus stop name enum with java doc

---
+++ Web/lab4/src/model/BusStop.java
@@ -0,0 +1,125 @@
+package model;
+
+import util.PassengerListGenerator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
+
+/**
+ * common resource for buses
+ */
+public class BusStop {
+
+    /**
+     * passengers list waiting on bus stop
+     *
+     */
+    private List<Passenger> passengers;
+
+
+    /**
+     * getter of passengers
+     *
+     */
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    /**
+     * exchanger passengers list going to next bus
+     *
+     */
+    private Exchanger<List<Passenger>> exchanger = new Exchanger<>();
+
+    /**
+     * getter of exchanger passengers
+     *
+     */
+    public Exchanger<List<Passenger>> getExchanger() {
+        return exchanger;
+    }
+
+    /**
+     * bus stop position
+     *
+     */
+    private Position position;
+
+    /**
+     * getter of bus stop position
+     *
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * bus stop name
+     *
+     */
+    private BusStopNameEnum name;
+
+    /**
+     * getter of bus stop name
+     *
+     */
+    public BusStopNameEnum getName() {
+        return name;
+    }
+
+    /**
+     * max buses can take place on bus stop at one time
+     *
+     */
+    private final int maxBusesAmount = 2;
+
+    /**
+     * synchronize semaphore buses on bus stop
+     *
+     */
+    private Semaphore semaphore = new Semaphore(maxBusesAmount,true);
+
+    /**
+     * getter of synchronize semaphore
+     *
+     */
+    public Semaphore getSemaphore() {
+        return semaphore;
+    }
+
+    /**
+     * list of buses, which occupied station
+     *
+     */
+    private ArrayList<Bus> busArrayList = new ArrayList<>(2);
+
+    /**
+     * getter of occupied station buses
+     *
+     */
+    public ArrayList<Bus> getBusArrayList() {
+        return busArrayList;
+    }
+
+
+    /**
+     * constructor of bus stop
+     * @param position position of bus stop
+     * @param busStopNameEnum name of bus stop
+     *
+     */
+    public BusStop(Position position, BusStopNameEnum busStopNameEnum) {
+        passengers = PassengerListGenerator.generatePassangers();
+        this.position = position;
+        this.name = busStopNameEnum;
+    }
+
+    @Override
+    public String toString() {
+        return "BusStop{" +
+                ", position=" + position +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
