0db234ed20763b67003f0089bb02e750e2a630ad
Ilya Sysoi <14ilya12322@gmail.com>
2018-11-25 01:19:47 +0300

    web: <<lab5_3>>: refactor server and handler

--- Web/lab5_3/src/by/isysoi/model/server/Server.java
+++ Web/lab5_3/src/by/isysoi/model/server/Server.java
@@ -1,16 +1,16 @@
 package by.isysoi.model.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.InetAddress;
 
 /**
  * Class of server that connect clinets and send images
@@ -24,94 +24,33 @@ public class Server {
 
     private static final int PORT = 9001;
 
-    private static Map<String, PrintWriter> clients = new HashMap<>();
-
     /**
      * Runs the server as an application
      * @param args args of comand line
      */
     public static void main(String[] args) {
         logger.info("The server is running on port: " + PORT) ;
-        try (ServerSocket listener = new ServerSocket(PORT)) {
-            while (true) {
-                new ServerHandler(listener.accept()).start();
-            }
-        }
-        catch (IOException e) {
-            logger.error(e);
-        }
-    }
+        EventLoopGroup group = new NioEventLoopGroup();
 
-    /**
-     * class of server handler that handling connections
-     *
-     * @author Ilya Sysoi
-     * @version 1.0
-     */
-    private static class ServerHandler extends Thread {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap
+                    .group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(InetAddress.getLocalHost(), PORT);
 
-
-        ServerHandler(Socket socket) {
-            this.socket = socket;
-        }
-
-
-        public void run() {
-            try {
-
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                out = new PrintWriter(socket.getOutputStream(), true);
-
-                while (true) {
-                    out.println("SUBMIT_NAME");
-                    name = in.readLine();
-                    if (name == null) {
-                        continue;
-                    }
-                    synchronized (clients) {
-                        if (clients.containsKey(name)) {
-                            logger.info("Client with name ( " + name +" ) is already connected");
-                        } else {
-                            logger.info("Client with name ( " + name +" ) is connected!");
-                            break;
-                        }
-                    }
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new ServerHandler());
                 }
-
-                out.println("NAME_ACCEPTED");
-                clients.put(name, out);
-
-                while (true) {
-                    String input = in.readLine();
-                    if (input == null) {
-                        return;
-                    }
-                    String[] rez = input.split(" image: ");
-                    if (clients.containsKey(rez[0])) {
-                        clients.get(rez[0]).println("IMAGE " + rez[1]);
-                        logger.info("Send image from ( " + name + " ) to ( " + rez[0] + " )");
-                    } else {
-                        out.println("FAIL_USER");
-                        logger.info("Client with name ( " + name +" ) doesn`t exists!");
-                    }
-
-                }
-            } catch (IOException e) {
-                System.out.println(e);
-            } finally {
-                if (name != null) {
-                    clients.remove(name);
-                    logger.info("SClient ( " + name + " ) removed");
-                }
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                }
-            }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            logger.warn(e);
+        } finally {
+            group.shutdownGracefully();
         }
     }
+
 }
---
+++ Web/lab5_3/src/by/isysoi/model/server/ServerHandler.java
@@ -0,0 +1,82 @@
+package by.isysoi.model.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * class of server handler that handling connections
+ *
+ * @author Ilya Sysoi
+ * @version 1.0
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private static Map<String, Channel> clients = new HashMap<>();
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws ServerException {
+        try {
+            super.channelRegistered(ctx);
+        } catch (Exception e) {
+            throw new ServerException("channelRegistered", e);
+        }
+        ctx.writeAndFlush(Unpooled.copiedBuffer("SUBMIT_NAME", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+        String input = ((ByteBuf) msg).toString(CharsetUtil.UTF_8).trim();
+
+        if (input.startsWith("NAME")) {
+            String[] rez = input.split("NAME ");
+            if (rez.length < 2) {
+                ctx.writeAndFlush(Unpooled.copiedBuffer("SUBMIT_NAME", CharsetUtil.UTF_8));
+            } else {
+                String name = rez[1];
+                if (clients.containsKey(name)) {
+                    logger.info("Client with name ( " + name + " ) is already connected");
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("SUBMIT_NAME", CharsetUtil.UTF_8));
+                } else {
+                    logger.info("Client with name ( " + name + " ) is connected!");
+                    clients.put(name, ctx.channel());
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("NAME_ACCEPTED", CharsetUtil.UTF_8));
+                }
+            }
+        } else if (input.startsWith("EXIT")) {
+            String[] rez = input.split("EXIT ");
+            String name = rez[1];
+
+            clients.remove(name);
+            ctx.channel().close();
+            logger.info("Client ( " + name + " ) removed");
+        } else {
+            String[] rez = input.split(" image: ");
+            if (clients.containsKey(rez[0])) {
+                clients.get(rez[0]).writeAndFlush(Unpooled.copiedBuffer("IMAGE " + rez[1], CharsetUtil.UTF_8));
+                logger.info("Send image to ( " + rez[0] + " )");
+            } else {
+                ctx.writeAndFlush(Unpooled.copiedBuffer("FAIL_USER", CharsetUtil.UTF_8));
+                logger.info("Client with name ( " + rez[0] + " ) doesn`t exists!");
+            }
+
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws ServerException {
+        ctx.close();
+        throw new ServerException("exceptionCaught", cause);
+    }
+}
