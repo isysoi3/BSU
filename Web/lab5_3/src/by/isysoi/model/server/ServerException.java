9e43b86022bb241296c69dad20e0536180e5b0d8
Ilya Sysoi <14ilya12322@gmail.com>
2018-11-25 01:19:18 +0300

    web: <<lab5_3>>: add exception

---
+++ Web/lab5_3/src/by/isysoi/model/server/ServerException.java
@@ -0,0 +1,37 @@
+package by.isysoi.model.server;
+
+/**
+ * Class representing server exception
+ * @author Ilya Sysoi
+ */
+public class ServerException extends Exception {
+
+
+    /**
+     * Constructor with specified string
+     * @param message string
+     */
+    public ServerException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor with specified string and exception
+     * @param message string
+     * @param e error covered
+     */
+    public ServerException(String message, Throwable e){
+        super(message, e);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+
+}
