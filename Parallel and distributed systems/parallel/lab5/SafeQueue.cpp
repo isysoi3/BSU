39e991ab96ba89461f30a3231f274f52a93733ce
Ilya Sysoi <14ilya12322@gmail.com>
2018-12-05 00:11:23 +0300

    parallel:: <<lab5>>: add standart queue

--- Parallel and distributed systems/parallel/lab5/SafeQueue.cpp
+++ Parallel and distributed systems/parallel/lab5/SafeQueue.cpp
@@ -4,26 +4,25 @@
 
 #include "SafeQueue.h"
 
-
 template<typename T>
-bool SafeQueue::empty()
+bool SafeQueue<T>::empty()
 {
     std::lock_guard<std::mutex> lock_guard(mutex);
     return queue.empty();
 }
 
 template<typename T>
-std::shared_ptr<T> SafeQueue::wait_and_pop()
+std::shared_ptr<T>  SafeQueue<T>::wait_and_pop()
 {
-    std::unique_lock<std::mutex> lg(mutex);
-    queue.wait(lg, !q.empty());
-    std::shared_ptr<T> result(std::make_shared<T>(move(q.front())));
+    std::unique_lock<std::mutex> unique_lock(mutex);
+    queue.wait(unique_lock, !queue.empty());
+    std::shared_ptr<T> result(std::make_shared<T>(move(queue.front())));
     queue.pop();
     return result;
 }
 
 template<typename T>
-std::shared_ptr<T> SafeQueue::try_pop()
+std::shared_ptr<T>  SafeQueue<T>::try_pop()
 {
     std::lock_guard<std::mutex> lock_guard(mutex);
     if(queue.empty())
@@ -33,13 +32,13 @@ std::shared_ptr<T> SafeQueue::try_pop()
     return result;
 }
 
-
-SafeQueue::SafeQueue(){
+template<typename T>
+SafeQueue<T>::SafeQueue(){
 
 }
 
 template<typename T>
-void SafeQueue::push(T element)
+void  SafeQueue<T>::push(T element)
 {
     std::lock_guard<std::mutex> lock_guard(mutex);
     queue.push(element);
--- Parallel and distributed systems/parallel/lab5/SafeQueue.h
+++ Parallel and distributed systems/parallel/lab5/SafeQueue.h
@@ -9,17 +9,18 @@
 #include <mutex>
 #include <memory>
 #include <condition_variable>
+#include <iostream>
 
 template<typename T>
 class SafeQueue {
 private:
     std::queue<T> queue;
-    mutable std::mutex mutex;
+    std::mutex mutex;
     std::condition_variable condition;
 public:
 
     SafeQueue();
-    void push(T element);
+    void push(T);
     bool empty();
     std::shared_ptr<T> wait_and_pop();
     std::shared_ptr<T> try_pop();
